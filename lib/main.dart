13aef9b7186094e82a352ecd0b79b88a4f9c2d9e
Minjee Song <minjeesong95@gmail.com>
2025-02-26 01:07:42 +0900

    code revised

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'modules/scan/scan_controller.dart';
 import 'modules/scan/scan_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ScanController()),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: ScanScreen(),
     );
   }
--- lib/modules/scan/scan_screen.dart
+++ lib/modules/scan/scan_screen.dart
@@ -5,22 +5,25 @@ import 'scan_controller.dart';
 class ScanScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final scanController = Provider.of<ScanController>(context);
-
     return Scaffold(
       appBar: AppBar(title: Text('Room Scanner')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () => scanController.startScan(),
-              child: Text("Start Scan"),
+      body: Builder(
+        builder: (context) {
+          final scanController = Provider.of<ScanController>(context);
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () => scanController.startScan(),
+                  child: Text("Start Scan"),
+                ),
+                SizedBox(height: 20),
+                Text(scanController.scanResult, textAlign: TextAlign.center),
+              ],
             ),
-            SizedBox(height: 20),
-            Text(scanController.scanResult, textAlign: TextAlign.center),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
